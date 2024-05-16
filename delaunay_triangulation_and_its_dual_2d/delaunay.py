Fix: Wrong results in liang_barsky_line_clipping

delaunay_triangulation_and_its_dual_2d/delaunay.py
@@ -15,7 +15,9 @@ logger = logging.getLogger(__name__)
 
 
 class Base(ABC):
-    def __init__(self, *, points: NDArray[np.float_]) -> None:
+    def __init__(
+        self, *, points: NDArray[np.float_], bounding_box: BoundingBox2d | None
+    ) -> None:
         """
 
         Args:
@@ -23,6 +25,8 @@ class Base(ABC):
                 A collection of "n" 2D points arranged in the Cartesian
                 coordinate system with the axis order (x, y) and having a shape
                 of (n, 2).
+            bounding_box:
+                An optional bounding box specifying the boundary of points
         """
         assert self._check_points_validity(points=points)
 
@@ -32,8 +36,16 @@ class Base(ABC):
         self._voronoi_tessellation_computed: bool = False
         self._barycentric_dual_computed: bool = False
 
-        self._bounding_box: BoundingBox2d = self._compute_bounding_box(
-            points=points
+        if bounding_box is not None and (
+            np.any(np.min(points, axis=0) < bounding_box.min_)
+            or np.any(np.max(points, axis=0) > bounding_box.max_)
+        ):
+            raise ValueError("The points are not within the bounding box")
+
+        self._bounding_box: BoundingBox2d = (
+            self._compute_bounding_box(points=points)
+            if bounding_box is None
+            else bounding_box
         )
 
         self._triangles: NDArray[np.intc]
@@ -386,8 +398,10 @@ class Base(ABC):
 
 
 class Delaunay(Base):
-    def __init__(self, *, points: NDArray[np.float_]) -> None:
-        super().__init__(points=points)
+    def __init__(
+        self, *, points: NDArray[np.float_], bounding_box: BoundingBox2d | None
+    ) -> None:
+        super().__init__(points=points, bounding_box=bounding_box)
 
         self.compute_delaunay_triangulation()
 

delaunay_triangulation_and_its_dual_2d/geometry/bounding_box_2d.py
@@ -91,21 +91,18 @@ class BoundingBox2d:
         t_exit = np.ones_like(x1)
 
         for i in range(4):
-            is_parallel = p[i] == 0
+            is_parallel = np.isclose(p[i], 0)
             outside_parallel = np.logical_and(is_parallel, q[i] < 0)
-            # t = np.where(p[i] != 0, q[i] / p[i], np.inf)
+            t_enter[outside_parallel] = np.nan
+            t_exit[outside_parallel] = np.nan
             # To avoid RuntimeWarning: divide by zero encountered in divide
             t = np.divide(
-                q[i], p[i], out=np.full_like(q[i], np.inf), where=p[i] != 0
+                q[i], p[i], out=np.full_like(q[i], np.inf), where=~is_parallel
             )
-            t_enter = np.where(
-                np.logical_and(p[i] < 0, ~outside_parallel),
-                np.maximum(t_enter, t),
-                t_enter,
-            )
+            t_enter = np.where(p[i] < 0, np.maximum(t_enter, t), t_enter)
             t_exit = np.where(p[i] > 0, np.minimum(t_exit, t), t_exit)
 
-        outside = t_enter > t_exit
+        outside = np.logical_or(np.isnan(t_enter), t_enter > t_exit)
         x1_clip = np.where(~outside, x1 + t_enter * dx, np.nan)
         y1_clip = np.where(~outside, y1 + t_enter * dy, np.nan)
         x2_clip = np.where(~outside, x1 + t_exit * dx, np.nan)

tests/delaunay/try.py
@@ -7,6 +7,7 @@ import scipy.spatial
 
 import delaunay_triangulation_and_its_dual_2d
 import delaunay_triangulation_and_its_dual_2d.exceptions
+import delaunay_triangulation_and_its_dual_2d.geometry
 
 
 def visualize_mocked_scipy_spatial_delaunay_and_voronoi():
@@ -58,12 +59,30 @@ def visualize_mocked_scipy_spatial_delaunay_and_voronoi():
 #     plt.show()
 
 
-def try_compute_bounded_line_segments_of_dual():
+def try_compute_bounded_line_segments_of_dual_from_predefined_points():
     points = np.array(
         [[0, 0], [3, 0], [1, 1], [3, 2], [4, 2], [1, 3], [2, 4], [4, 4]],
         dtype=np.float32,
     )
     delaunay = delaunay_triangulation_and_its_dual_2d.Delaunay(points=points)
+    bounded_line_segments = delaunay.compute_bounded_line_segments_of_dual(
+        dual="voronoi"
+    )
+    print("bounded_line_segments.shape: ", bounded_line_segments.shape)
+
+
+def try_compute_bounded_line_segments_of_dual_from_random_points():
+    lower_bound = 0.0
+    upper_bound = 1.0
+    rng = np.random.default_rng()
+    points = rng.uniform(low=lower_bound, high=upper_bound, size=(10, 2))
+    delaunay = delaunay_triangulation_and_its_dual_2d.Delaunay(
+        points=points,
+        bounding_box=delaunay_triangulation_and_its_dual_2d.geometry.BoundingBox2d(
+            min_=np.array([lower_bound, lower_bound], dtype=np.float_),
+            max_=np.array([upper_bound, upper_bound], dtype=np.float_),
+        ),
+    )
     bounded_line_segments = delaunay.compute_bounded_line_segments_of_dual(
         dual="voronoi"
     )